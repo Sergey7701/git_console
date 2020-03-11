3fb1a74bade76345ae12298dad1a08537f9bb95b
sergey <imperium-romanum@mail.ru>
2020-03-11 15:49:51 +0300

    command delete

--- src/TestMethod.php
+++ src/TestMethod.php
@@ -1,28 +1,2 @@
 <?php
 namespace App;
-
-// src/Command/CreateUserCommand.php
-
-
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-
-
-class TestMethod extends Command
-{
-
-    // the name of the command (the part after "bin/console")
-    protected static $defaultName = 'app:create-user';
-
-    protected function configure()
-    {
-          $this->setname('git test');
-    }
-
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        $output->writeln('test');
-        return 0;
-    }
-}
