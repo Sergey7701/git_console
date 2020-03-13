ab8a183a9c1ecca64fc4cbeaebed43e3b30fbe29
sergey <imperium-romanum@mail.ru>
2020-03-13 16:04:19 +0300

    rename class

--- src/PrintString.php
+++ src/PrintString.php
@@ -9,7 +9,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 
-class TestMethod extends Command
+class PrintString extends Command
 {
 
     // the name of the command (the part after "bin/console")
